fa3b1e14b743b2a8721752e06121cb1a05a6dd39
SupriyaWalunj <supriyawalunj03@gmail.com>
2023-10-25 09:24:36 +0530

    commited to develop branch

--- src/test/java/resources/GitExample.java
+++ src/test/java/resources/GitExample.java
@@ -11,4 +11,17 @@ public class GitExample {
 		System.out.println("enter value 2");
 	}
 	
+	public void example3()
+	{
+		System.out.println("enter value 2");
+	}
+	public void example4()
+	{
+		System.out.println("enter value 2");
+	}
+	public void example5()
+	{
+		System.out.println("enter value 2");
+	}
+	
 }
